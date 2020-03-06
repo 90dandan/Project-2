ce944a6b99af38cdcb404a3201f7862f874f9c0a
90dandan <1brokendrumhead@gmail.com>
2020-03-06 09:33:01 -0600

    MVP CRUD

--- routes/comments.js
+++ routes/comments.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const commentsCtrl = require('../controller/comments');
 
 router.post('/:id/comments', isLoggedIn, commentsCtrl.create);
+router.delete('/:id/comments/:cid', isLoggedIn, commentsCtrl.delete);
+router.put('/:id/comments/:cid', isLoggedIn, commentsCtrl.update);
 
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) return next();
